885885024f4c23d045073493505c199471648f05
jhampton14 <jhampton14@murraystate.edu>
2023-09-20 12:18:47 -0500

    Updating according to BookSorter.java

--- BookDatabaseSorting.java
+++ BookDatabaseSorting.java
@@ -1,6 +1,8 @@
 // Jonah H. and Maria D.
 // CSC 325 FA23; Subtask 6
 
+import java.util.List;
+
 public class BookDatabaseSorting extends BookDatabase {
     // Creating an instance of Sortable interface which now has commands from BookSorter
     private Sortable sorter;
@@ -12,23 +14,23 @@ public class BookDatabaseSorting extends BookDatabase {
 
     // Calling the methods from Sortable which will return the sorted ArrayList
     // a.k.a. the integration of methods
-    public void passCategory() {
+    public void passCategory(List<Book> books) {
         sorter.sortByCategory(books);
     }
 
-    public void passAuthor() {
+    public void passAuthor(List<Book> books) {
         sorter.sortByAuthor(books);
     }
 
-    public void passTitle() {
+    public void passTitle(List<Book> books) {
         sorter.sortByTitle(books);
     }
 
-    public void passLength() {
+    public void passLength(List<Book> books) {
         sorter.sortByLength(books);
     }
 
-    public void passPopularity() {
+    public void passPopularity(List<Book> books) {
         sorter.sortByPopularity(books);
     }
 }   
