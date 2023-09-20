a892f16d10fb895061f5166463575fe3f93b3367
cha0sdrive <gundersonisaac992@gmail.com>
2023-09-20 08:38:31 -0500

    modified getListFromInput class

--- getListFromInput.java
+++ getListFromInput.java
@@ -2,33 +2,40 @@
 import java.util.List;
 
 public class getListFromInput {
-    
+    Sortable mySorter;
+
+    public getListFromInput(Sortable sorter) {
+        this.mySorter = sorter;
+    }
+
+    private BookDatabaseSorting databaseSorter = new BookDatabaseSorting(mySorter);
+
     //method for getting the sort based on user input
     public List<Book> getSortedBooks(String category) {
 
     if (category == "Title") {       //get list sorted by title
 
-        return(passTitle());      //call bookDatabaseSorting class methods
+        return(databaseSorter.passTitle());      //call bookDatabaseSorting class methods
     }
 
     if (category == "Author") {      //get list sorted by author
 
-        return(passAuthor());     //call bookDatabaseSorting class methods
+        return(databaseSorter..passAuthor());     //call bookDatabaseSorting class methods
     }
 
     if (category == "Length") {      //get list sorted by pagenumber
 
-        return(passLength());     //call bookDatabaseSorting class methods
+        return(databaseSorter.passLength());     //call bookDatabaseSorting class methods
     }
 
     if (category == "Popularity") {      //get list sorted by rating
 
-        return(passPopularity());     //call bookDatabaseSorting class methods
+        return(databaseSorter.passPopularity());     //call bookDatabaseSorting class methods
     }
 
     if (category == "Category") {      //get list sorted by category
 
-        return(passCategory());   //call bookDatabaseSorting class methods
+        return(databaseSorter.passCategory());   //call bookDatabaseSorting class methods
     }
 
     }
