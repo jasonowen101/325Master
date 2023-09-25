f6b6a642897eff81d5d5c715fd6ea7057759ead1
Ashen White <swhite49@murraystate.edu>
2023-09-25 08:56:09 -0500

    removed key

--- BookDatabase.java
+++ BookDatabase.java
@@ -3,35 +3,23 @@ import java.util.ArrayList;
 public class BookDatabase {
 	
 	private ArrayList<Book> database = new ArrayList<Book>();
-	private ArrayList<Integer> key = new ArrayList<Integer>();
-	private int keyNext;
 	// database is an array list containing objects of the Book class
-	// key is an array list containing integers matching each book
-	// keyNext increments every time a book is added
 	public void addBook(Book book) {
 		database.add(book);
-		keyNext++;
-		key.add(keyNext);
 	}
-	// addBook takes a Book object, appends it to the list, and adds a matching key
-	public boolean removeBook(int i) {
-		int index = key.indexOf(i);
-		if (index == -1) {
-			return false;
-		}
-		database.remove(index);
-		key.remove(index);
-		return true;
+	// addBook takes a Book object, appends it to the list
+	public void removeBook(int i) {
+		database.remove(i);
 	}
-	// removeBook removes a book of a key, and removes the key
+	// removeBook removes a book at an index
 	public Book retrieveBook(int i) {
-		int index = key.indexOf(i);
-		if(index == -1) {
-			return null;
-		}
-		return database.get(index);
+		return database.get(i);
 	}
-	// retrieveBook returns a book object of a key, or null if the key does not exist
+	// retrieveBook returns a book object at an index
+	public int size() {
+		return database.size();
+	}
+	// size returns the size of the database
     public String toString() {
         return database.toString();
     }
